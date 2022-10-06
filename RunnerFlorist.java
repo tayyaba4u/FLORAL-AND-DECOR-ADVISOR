d565ce882be9b669f1273d8aa34d250b8cf61fb4
tayyaba4u <113998279+tayyaba4u@users.noreply.github.com>
2022-10-06 21:00:05 +0500

    Add files via upload
    Here is the runner java file for Florist and Decor

---
+++ RunnerFlorist.java
@@ -0,0 +1,13 @@
+public class RunnerFlorist {
+    public static void main (String args [])
+{
+FloristandDecor flower = new FloristandDecor ();
+System.out.println(flower.CalculateFloralBudget());
+
+System.out.println(flower.RecommendedFloristsandDecors());
+
+}
+    
+}
+
+    
